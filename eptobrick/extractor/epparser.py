8ea76c17296cbaf7f32ae6e2ce052508a63f3e4e
ThibaultMarzullo <thib.marzullo@gmail.com>
2022-10-12 15:57:46 -0600

    Added E+.idd and epjson

--- eptobrick/extractor/epparser.py
+++ eptobrick/extractor/epparser.py
@@ -16,10 +16,10 @@ class Extractor():
     def __init__(self, verbose=3) -> None:
         m.debug = verbose
         self.graph = Graph()
-        self.BRICK = Namespace('https://brickschema.org/schema/1.0.1/Brick#')
-        self.BF = Namespace('https://brickschema.org/schema/1.0.1/BrickFrame#')
+        self.BRICK = Namespace('https://brickschema.org/schema/Brick#') #1.0.1 is not supported anymore
+        self.BF = Namespace('https://brickschema.org/schema/Brick#') #BrickFrame -> BRICK
         self.graph.bind('brick', self.BRICK)
-        self.graph.bind('bf', self.BF)
+        #self.graph.bind('bf', self.BF)
         self.graph.bind('rdfs', RDFS)
         self.graph.bind('rdf', RDF)
 
@@ -27,7 +27,7 @@ class Extractor():
         '''
         Load an IDF file.
 
-        Parameters:
+        Args:
         eppath : str
             Path to the IDF
         Returns:
